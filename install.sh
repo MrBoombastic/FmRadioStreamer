f5aecf61bb73d1554d4d0ccbc8c5bc3155be5a09
MrBoombastic <mr.tolek2003@gmail.com>
2020-05-12 18:29:18 +0200

    let's save some disk space!

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install ffmpeg libsndfile1-dev && echo "Successfully installed modules!" || echo "Failed to install modules!"
-git clone https://github.com/miegl/PiFmAdv temp/ && echo "Successfully clone PiFmAdv repo!" || echo "Failed to clone PiFmAdv repo!"
+rm core/placeholder.txt
+git clone https://github.com/miegl/PiFmAdv temp/ && echo "Successfully cloned PiFmAdv repo!" || echo "Failed to clone PiFmAdv repo!"
 mv temp/src/* core/ && echo "Moved it to core" || echo "Failed to move it to core"
 sudo rm -r temp/
 cd core && make clean && echo "Core cleaned." || echo "Failed to clean core."
