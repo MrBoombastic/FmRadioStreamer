79b81f3a6cc59e7ad7d616cd52fbc37eb4a9d7c6
MrBoombastic <mr.tolek2003@gmail.com>
2020-05-12 18:04:10 +0200

    test: clone and copy pifmadv by install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo apt-get update
 sudo apt-get install ffmpeg libsndfile1-dev && echo "Successfully installed modules!" || echo "Failed to install modules!"
+git clone https://github.com/miegl/PiFmAdv temp/ && echo "Successfully clone PiFmAdv repo!" || echo "Failed to clone PiFmAdv repo!"
+mv temp/src/* core/ && echo "Moved it to core" || echo "Failed to move it to core"
+sudo rm -r temp/
 cd core && make clean && echo "Core cleaned." || echo "Failed to clean core."
 make && echo "Successfully compiled!" || echo "Failed to compile!"
 cd ../
