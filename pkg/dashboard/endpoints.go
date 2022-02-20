d3f6892104d205be02cc5dd3677ddbe8bec61b45
MrBoombastic <contact@amroz.xyz>
2022-02-20 21:12:41 +0100

    better error logging in endpoints

--- pkg/dashboard/endpoints.go
+++ pkg/dashboard/endpoints.go
@@ -15,21 +15,33 @@ import (
 func music(ctx *fiber.Ctx) {
 	filesSlice, err := condlers.MusicDir()
 	if err != nil {
-		ctx.SendStatus(500)
+		err := ctx.SendStatus(500)
+		if err != nil {
+			log.Println(err)
+		}
 	}
-	ctx.JSON(filesSlice)
+	err = ctx.JSON(filesSlice)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 // stop endpoint plays silence
 func stop(ctx *fiber.Ctx) {
 	core.Play("")
-	ctx.SendStatus(200)
+	err := ctx.SendStatus(200)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 // offair kill PiFmAdv entirely
 func offair(ctx *fiber.Ctx) {
 	core.Kill()
-	ctx.SendStatus(200)
+	err := ctx.SendStatus(200)
+	if err != nil {
+		log.Println(err)
+	}
 	if config.GetSSD1306() {
 		ssd1306.MiniMessage("OFF-AIR")
 	}
@@ -42,15 +54,24 @@ func yt(ctx *fiber.Ctx) {
 	result, err := tools.SearchYouTube(query)
 	if err != nil {
 		log.Println(err)
-		ctx.SendStatus(500)
+		err := ctx.SendStatus(500)
+		if err != nil {
+			log.Println(err)
+		}
 		return
 	}
 	if search == "true" {
-		ctx.JSON(result.Items[0].Snippet)
+		err := ctx.JSON(result.Items[0].Snippet)
+		if err != nil {
+			log.Println(err)
+		}
 	} else {
-		ctx.SendStatus(200)
+		err := ctx.SendStatus(200)
+		if err != nil {
+			log.Println(err)
+		}
 		leds.BlueLedEnabled = true
-		err := condlers.DownloadWav("https://youtu.be/" + result.Items[0].ID.VideoID)
+		err = condlers.DownloadWav("https://youtu.be/" + result.Items[0].ID.VideoID)
 		leds.BlueLedEnabled = false
 		if err != nil {
 			log.Println(err)
@@ -61,9 +82,12 @@ func yt(ctx *fiber.Ctx) {
 // download endpoint performs downloading audio from other sites
 func download(ctx *fiber.Ctx) {
 	query := ctx.Query("q")
-	ctx.SendStatus(200)
+	err := ctx.SendStatus(200)
+	if err != nil {
+		log.Println(err)
+	}
 	leds.BlueLedEnabled = true
-	err := condlers.DownloadWav(query)
+	err = condlers.DownloadWav(query)
 	leds.BlueLedEnabled = false
 	if err != nil {
 		log.Println(err)
@@ -74,7 +98,10 @@ func download(ctx *fiber.Ctx) {
 func play(ctx *fiber.Ctx) {
 	query := ctx.Query("q")
 	core.Play(query)
-	ctx.SendStatus(200)
+	err := ctx.SendStatus(200)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 // save endpoint updates current config
@@ -87,11 +114,17 @@ func save(ctx *fiber.Ctx) {
 	if config.GetSSD1306() {
 		ssd1306.Refresh()
 	}
-	ctx.SendStatus(200)
+	err := ctx.SendStatus(200)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 // configuration endpoint returns current config
 func configuration(ctx *fiber.Ctx) {
 	configMap := config.GetMap()
-	ctx.JSON(configMap)
+	err := ctx.JSON(configMap)
+	if err != nil {
+		log.Println(err)
+	}
 }
