dcf85db61aafece6346d14ef2c368732d1b4d9f8
MrBoombastic <contact@amroz.xyz>
2022-02-22 16:37:54 +0100

    wav test

--- pkg/condlers/condlers_test.go
+++ pkg/condlers/condlers_test.go
@@ -22,3 +22,25 @@ func TestMusicDir(t *testing.T) {
 		t.Errorf("got %v, wanted %v", got, wanted)
 	}
 }
+
+func TestDownloadWav(t *testing.T) {
+	err := os.Chdir("../../")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = DownloadWav("https://www.youtube.com/watch?v=lp7zvP4GxQA")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	got, err := MusicDir()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	wanted := []string{"Jarre arr. by Rob Hubbard - Zoolook (Oscilloscope View).wav", "piano-kozco-com.wav"}
+	if !reflect.DeepEqual(got, wanted) {
+		t.Errorf("got %v, wanted %v", got, wanted)
+	}
+}
