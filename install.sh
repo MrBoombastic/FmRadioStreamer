397abadca35ddb68939f8817beceed880dadd916
MrBoombastic <contact@amroz.xyz>
2022-11-24 19:08:42 +0100

    small rewording

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ mkdir "core"
 mkdir "music"
 echo "FMRADSTR: Downloading PiFmAdv..."
 git submodule update --init --recursive && echo "FMRADSTR: Done!" || echo "FMRADSTR: Not a git repository! Downloading directly..." && git clone https://github.com/miegl/PiFmAdv.git && git checkout 7562fe79789ec4cc0d209a7b14b07d79cdf6e310
-echo "FMRADSTR: Moving and compiling PiFmAdv..."
+echo "FMRADSTR: Compiling PiFmAdv..."
 mv PiFmAdv/src/* core/ && echo "FMRADSTR: Moved it to core directory!" || echo "FMRADSTR: Failed to move it to core directory!"
 cd core && make clean && echo "FMRADSTR: Core cleaned!" || echo "FMRADSTR: Failed to clean core!"
 make && echo "FMRADSTR: Successfully compiled!" || echo "FMRADSTR: Failed to compile!"
 cd ../
-echo "FMRADSTR: Done. Please set YT API key in config.json and DO NOT FORGET to add 'gpu_freq=250' in /boot/config.txt."
+echo "FMRADSTR: Done. Please set YT API key in config.json and DO NOT FORGET to add 'gpu_freq=250' in /boot/config.txt!"
 echo "FMRADSTR WARNING: Make sure to always use up-to-date version of youtube_dl!"
 echo "FMRADSTR: Enjoy! :)"
